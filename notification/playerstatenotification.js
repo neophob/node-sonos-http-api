138bd476cb6201bd3c89ffb9cc9ff30f4eed8cdb
Michael Vogt <michu@neophob.com>
2014-07-07 14:40:59 +0200

    refactoring notification

---
+++ notification/playerstatenotification.js
@@ -0,0 +1,20 @@
+
+function PlayerstateNotification(discovery, notificationApi, pollingTime) {
+ setInterval(function() {
+    discovery.getZones().forEach(function (zone) {
+      var state = zone.coordinator.state;
+      var albumArtUri = state.currentTrack.albumArtURI;
+      if (albumArtUri.startsWith('/')) {
+        var playeraddress = discovery.getPlayerByUUID(zone.coordinator.uuid).address;
+        var albumUrl = 'http://' + playeraddress + ':1400' + albumArtUri;
+        state.currentTrack.albumArtURI = albumUrl;
+      }
+      if (JSON.stringify(storedState) !== JSON.stringify(state)) {
+        storedState = JSON.parse(JSON.stringify(state));
+        notificationApi.sendPlayerstateChangedNotification(state);
+      }
+    });
+  }, pollingTime);
+}
+
+module.exports = PlayerstateNotification;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var SonosDiscovery = require('sonos-discovery');
 var SonosHttpAPI = require('./lib/sonos-http-api.js');
-var NotificationAPI = require('./lib/notification-service.js');
+var NotificationAPI = require('./notification/notification-service.js');
+var PlayerstateNotification = require('./notification/playerstatenotification.js');
 var fs = require('fs');
 var conf = require('./conf');
 var log = require('./lib/log')('sonosserver');
@@ -13,33 +14,16 @@ var presets = {};
 var storedState = {};
 
 fs.exists('./presets.json', function (exists) {
-	if (exists) {
-		presets = require('./presets.json');
+  if (exists) {
+    presets = require('./presets.json');
     log.info('loaded presets', presets);
-	}
-  var sonosHttpAPI = new SonosHttpAPI(discovery, conf.get('rest.ip'), conf.get('rest.port'), presets, log);
-
-  setInterval(function() {
-    discovery.getZones().forEach(function (zone) {
-      var state = zone.coordinator.state;
-      var albumArtUri = state.currentTrack.albumArtURI;
-      if (albumArtUri.startsWith('/')) {
-        var playeraddress = discovery.getPlayerByUUID(zone.coordinator.uuid).address;
-        var albumUrl = 'http://' + playeraddress + ':1400' + albumArtUri;
-        state.currentTrack.albumArtURI = albumUrl;
-      }
-      if (JSON.stringify(storedState) !== JSON.stringify(state)) {
-        storedState = JSON.parse(JSON.stringify(state));
-        notificationApi.sendPlayerstateChangedNotification(state);
-      }
-    });
-  }, conf.get('polling.time'));
+  }
 });
-
+var sonosHttpAPI = new SonosHttpAPI(discovery, conf.get('rest.ip'), conf.get('rest.port'), presets, log);
+new PlayerstateNotification(discovery, notificationApi, conf.get('polling.time'));
 
 process.on('SIGHUP', function() {
   log.info('SIGHUP, not implemented yet!');
-  //TODO maybe reparse process.env?
 });
 process.on('uncaughtException', function(err) {
   log.error('uncaughtException detected! Exit application with Errorcode 1!');
