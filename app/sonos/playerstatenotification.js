19c84ae2f71f0f4003e45df50c4d43154b7eb10c
Michael Vogt <michu@neophob.com>
2014-07-14 21:20:10 +0200

    notifications: handle multiple rooms

--- app/sonos/playerstatenotification.js
+++ app/sonos/playerstatenotification.js
@@ -1,8 +1,9 @@
 var storedState = {};
 
 function PlayerstateNotification(discovery, notificationApi, pollingTime) {
- setInterval(function() {
+  setInterval(function() {
     discovery.getZones().forEach(function (zone) {
+      var roomName = zone.coordinator.roomName;
       var state = zone.coordinator.state;
       var albumArtUri = state.currentTrack.albumArtURI;
       if (albumArtUri.startsWith('/')) {
@@ -10,8 +11,13 @@ function PlayerstateNotification(discovery, notificationApi, pollingTime) {
         var albumUrl = 'http://' + playeraddress + ':1400' + albumArtUri;
         state.currentTrack.albumArtURI = albumUrl;
       }
-      if (JSON.stringify(storedState) !== JSON.stringify(state)) {
-        storedState = JSON.parse(JSON.stringify(state));
+
+      if (storedState[roomName] === undefined) {
+        storedState[roomName] = JSON.parse(JSON.stringify(state));
+      }
+
+      if (JSON.stringify(storedState[roomName]) !== JSON.stringify(state)) {
+        storedState[roomName] = JSON.parse(JSON.stringify(state));
         notificationApi.sendPlayerstateChangedNotification(state);
       }
     });
